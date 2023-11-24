75081e905690dc1f65260ecaffcefab55396ef24
frenchpig <auspont.javier@gmail.com>
2023-11-24 15:20:56 -0300

    deleted test code

--- public/js/absences.js
+++ public/js/absences.js
@@ -2,8 +2,6 @@ document.getElementById('absences').addEventListener('click', function() {
   window.location.href='/absences'
 });
 
-console.log("TEST");
-
 function filter(text){
   window.location.href = `/absences/filter/${text}`
 }
